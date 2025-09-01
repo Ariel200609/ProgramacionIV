ecd4acd201a9b501d4a78e2407eb40881b614f52
Ariel200609 <arielmontoya200609@gmail.com>
2025-09-01 15:19:32 -0300

    ejercicio4Gian

---
+++ tp1/ejercicio4/index.ts
@@ -0,0 +1,9 @@
+import { Pajaro } from "./pajaro"
+import { Zorro } from "./zorro"
+
+const p = new Pajaro("aguila", "ave")
+p.hacerSonido()
+p.volar()
+
+const z = new Zorro("zorro", "rojo")
+z.hacerSonido()
